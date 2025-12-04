3e568b06ea01b7d13df6f62a3bb01c5e86481a2d
zyansaber <zyan112491@gmail.com>
2025-12-05 08:54:04 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Toaster } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Index from './pages/Index';
+import DispatchPage from './pages/DispatchPage';
+import ReallocationPage from './pages/ReallocationPage';
+import StockPage from './pages/StockPage';
 import NotFound from './pages/NotFound';
 
 const App = () => (
@@ -9,7 +12,12 @@ const App = () => (
     <Toaster />
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path="/" element={<Index />}>
+          <Route index element={<Navigate to="/stock" replace />} />
+          <Route path="stock" element={<StockPage />} />
+          <Route path="dispatch" element={<DispatchPage />} />
+          <Route path="reallocation" element={<ReallocationPage />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
