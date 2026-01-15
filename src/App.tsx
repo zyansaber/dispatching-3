980e691e330cb81b60490d62a2088705ea9711c1
zyansaber <zyan112491@gmail.com>
2026-01-15 16:44:26 +1100

    Revert "Preview PDFs and embed photos in transport damage report PDFs"

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import ReallocationPage from './pages/ReallocationPage';
 import StockPage from './pages/StockPage';
 import AdminPage from './pages/AdminPage';
 import PrintDocPage from './pages/PrintDocPage';
-import TransportDamageRecordPage from './pages/TransportDamageRecordPage';
 import NotFound from './pages/NotFound';
 
 const App = () => (
@@ -20,7 +19,6 @@ const App = () => (
           <Route path="stock" element={<StockPage />} />
           <Route path="dispatch" element={<DispatchPage />} />
           <Route path="print-doc" element={<PrintDocPage />} />
-          <Route path="transport-damage" element={<TransportDamageRecordPage />} />
           <Route path="reallocation" element={<ReallocationPage />} />
           <Route path="admin" element={<AdminPage />} />
         </Route>
